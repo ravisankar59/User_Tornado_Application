33c08d0f673aa73f5eec86c7999589e817da7796
ravisankar <mamidi.ravisankar559@gmail.com>
2016-03-17 19:18:47 +0530

    second commit

--- config.py
+++ config.py
@@ -4,9 +4,35 @@ from tornado.ioloop import IOLoop
 import sqlite3 as sqlite
 import tornado.web
 
-def execute(query):
+def dict_gen(query):
+    print query
+    connection = sqlite.connect('Users')
+    cursorobj = connection.cursor()
+    cursorobj.execute(query)
+    print(dir(cursorobj))
+    print(cursorobj.description)
+    ''' From Python Essential Reference by David Beazley'''
+    import itertools
+    field_names = [d[0].lower() for d in cursorobj.description]
+    while True:
+        print cursorobj
+        rows = cursorobj.fetchma()
+        if not rows: return
+        for row in rows:
+            yield dict(itertools.izip(field_names, row))
+    # connection.commit()
+    # connection.close()
 
-        connection = sqlite.connect('User')
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def get_all(query):
+
+        connection = sqlite.connect('Users')
+        connection.row_factory = dict_factory
         cursorobj = connection.cursor()
         # print('EXECUTE ')
         try:
@@ -19,9 +45,42 @@ def execute(query):
         connection.close()
         return result
 
+def get_one(query):
+
+        connection = sqlite.connect('Users')
+        connection.row_factory = dict_factory
+        cursorobj = connection.cursor()
+        # print('EXECUTE ')
+        try:
+            cursorobj.execute(query)
+            result = cursorobj.fetchone()
+                # print(result)
+            connection.commit()
+        except Exception:
+                        raise
+        connection.close()
+        return result
+
+def create(query):
+
+        connection = sqlite.connect('Users')
+        connection.row_factory = dict_factory
+        cursorobj = connection.cursor()
+        # print('EXECUTE ')
+        try:
+            cursorobj.execute(query)
+            result = cursorobj.fetchone()
+            # result = cursorobj.fetchone()
+            #     # print(result)
+            connection.commit()
+        except Exception:
+                        raise
+        connection.close()
+        return result
+
 
 def UserDatabase():
-    conn = sqlite.connect('User')
+    conn = sqlite.connect('Users')
     c = conn.cursor()
     try:
         c.execute('SELECT * FROM person')
--- handlers.py
+++ handlers.py
@@ -4,16 +4,14 @@ from tornado.ioloop import IOLoop
 import sqlite3 as sqlite
 import tornado.web
 import json
-from config import execute
+from config import get_all,get_one, create
 
 
 class AddUserHandler(tornado.web.RequestHandler):
     def get(self):
         query = '''SELECT * FROM person'''
-        result = execute(query)
-        print(result)
-        
-        self.write(json.dumps(dict(response=result)))
+        result = get_all(query)
+        self.write(json.dumps(dict(users=result)))
 
     def post(self):
         data = json.loads(self.request.body)
@@ -21,34 +19,28 @@ class AddUserHandler(tornado.web.RequestHandler):
         lname = data.get('lastname')
         phno = data.get('mobilenumber')
         address = data.get('address')
-        query = ''' insert into person (firstname,lastname,mobilenumber,address) values ("{0}","{1}","{2}","{3}") '''.format(fname,lname,phno,address) ;
-        result = execute(query)
-        self.write(json.dumps(dict(response="Success")))
+        query = ''' INSERT INTO person (firstname,lastname,mobilenumber,address) VALUES ("{0}","{1}","{2}","{3}") '''.format(fname,lname,phno,address) ;
+        result = create(query)
+        self.write(json.dumps(dict(message="successfully added user:{0} {1}".format(fname, lname))))
 
 class SingleUserHandler(tornado.web.RequestHandler):
 
     def get(self, user_id):
-        query = '''select * from person where id="{0}"'''.format(user_id);
-        result = execute(query)
-        if len(result) == 0:
-            result = "User does not exist with the given ID {0}".format(user_id)
+        query = '''SELECT * FROM person WHERE id="{0}"'''.format(user_id);
+        result = get_one(query)
+        if result is None:
+            result = "User does not exist with the ID: {0}".format(user_id)
 
-        self.write(json.dumps(dict(response=result)))
+        self.write(json.dumps(result))
 
     def put(self, user_id):
-        # print "In PUT"
-        data = json.loads(self.request.body)
-        uid = data.get('id')
-        fname = data.get('firstname')
-        lname = data.get('lastname')
-        phno = data.get('mobilenumber')
-        address = data.get('address')
-        query = ''' update person set address="{1}" where id="{0}"'''.format(user_id,address);
-        result = execute(query)
-        print result
+        params = json.loads(self.request.body)
+        query_params = ",".join('''"{0}"="{1}"'''.format(key, params[key]) for key in params if params[key]is not None)
+        query = ''' UPDATE person SET {0} WHERE id="{1}"'''.format(query_params,user_id);
+        result = create(query)
         self.write(json.dumps(dict(result="Success")))
 
     def delete(self, user_id):
-        query = '''delete  from person where id = "{0}"'''.format(user_id);
-        result = execute(query)
-        self.write(json.dumps(dict(response=result)))
+        query = '''DELETE FROM person WHERE id = "{0}"'''.format(user_id);
+        result = create(query)
+        self.write(json.dumps(dict(message="Successfully deleted user with id: {0}".format(user_id))))
